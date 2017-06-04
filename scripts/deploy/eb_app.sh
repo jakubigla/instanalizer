21bb7fb6ecd1c459d6ddd105c9bca9e21a6de281
Jakub Igla <jakub.igla@gmail.com>
2017-06-04 19:22:05 +0100

    Add deployment stage

--- scripts/deploy/eb_app.sh
+++ scripts/deploy/eb_app.sh
@@ -6,5 +6,13 @@ source "$SCRIPTS_DIR/functions.sh"
 APPLICATION_NAME="$1"
 ENVIRONMENT_NAME="$2"
 
-aws elasticbeanstalk describe-environments --application-name "$APPLICATION_NAME"
+ENV_EXISTS=`aws elasticbeanstalk describe-environments \
+    --application-name "$APPLICATION_NAME" \
+    --environment-names "$ENVIRONMENT_NAME" \
+    | jq '.Environments | length'`
 
+if [ "$ENV_EXISTS" -eq 0 ]; then
+    eb create "$ENVIRONMENT_NAME"
+else
+    eb deploy "$ENVIRONMENT_NAME"
+fi
